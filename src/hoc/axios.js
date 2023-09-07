6d97e091533971071883cf6a5eee1fafb5fa2e24
DeepakFromZeus <manedeep2001@gmail.com>
2023-09-08 00:45:04 +0530

    baseurl changes

--- src/hoc/axios.js
+++ src/hoc/axios.js
@@ -8,9 +8,11 @@ const axiosInstance = axios.create({
 
   // baseURL: 'http://16.171.39.169:3100'
 
-  baseUrl: 'https://api.inventorysolutions.in'
+  baseURL: 'https://api.inventorysolutions.in'
 
 })
+
+console.log("sdfsdf " + axiosInstance.defaults.baseURL);
 
 axiosInstance.interceptors.request.use(
   config => {
